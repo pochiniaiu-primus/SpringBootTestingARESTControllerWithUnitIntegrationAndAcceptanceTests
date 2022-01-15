c706b9a9483f44ea894035725c3fd7d354e6b649
Serhii Honchar <sehonc@softserveinc.com>
2022-01-15 19:54:47 +0200

    refactor unit test for WelcomeController using Mockito

--- src/main/java/com/serhiihonchar/testingrestcontroller/controller/WelcomeController.java
+++ src/main/java/com/serhiihonchar/testingrestcontroller/controller/WelcomeController.java
@@ -1,13 +1,24 @@
 package com.serhiihonchar.testingrestcontroller.controller;
 
+import com.serhiihonchar.testingrestcontroller.service.WelcomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    // /welcome?name=John
+
+    private WelcomeService welcomeService;
+
+    @Autowired
+    public WelcomeController(WelcomeService welcomeService) {
+        this.welcomeService = welcomeService;
+    }
 
     @GetMapping("/welcome")
-    public String welcome(String name) {
-        return String.format("Welcome %s!", name);
+    public String welcome(@RequestParam(defaultValue = "Stranger") String name) {
+        return welcomeService.getWelcomeMessage(name);
     }
 }
--- src/test/java/com/serhiihonchar/testingrestcontroller/controller/WelcomeControllerUnitTest.java
+++ src/test/java/com/serhiihonchar/testingrestcontroller/controller/WelcomeControllerUnitTest.java
@@ -1,14 +1,19 @@
 package com.serhiihonchar.testingrestcontroller.controller;
 
+import com.serhiihonchar.testingrestcontroller.service.WelcomeService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class WelcomeControllerUnitTest {
 
     @Test
     void shouldWelcome() {
-        WelcomeController welcomeController = new WelcomeController();
+        WelcomeService welcomeService = Mockito.mock(WelcomeService.class);
+        when(welcomeService.getWelcomeMessage("Serhii")).thenReturn("Welcome Serhii!");
+        WelcomeController welcomeController = new WelcomeController(welcomeService);
         assertEquals("Welcome Serhii!", welcomeController.welcome("Serhii"));
     }
 }
